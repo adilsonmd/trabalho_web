666c27146e8c395919842d113db0251656d474cb
Adilson Alves <adilson.day@hotmail.com>
2016-05-07 19:31:28 -0300

    Pagina listar adicionada
    porém ainda não funcional

--- src/inc_conn.php
+++ src/inc_conn.php
@@ -8,7 +8,7 @@
 	<body>
 	<ul>
 		<li><a href="incluir.php">Incluir funcionário</a></li>
-		<li>Listar funcionário</li>
+		<li><a href="listar.php">Listar funcionário</a></li>
 		<li>Alterar funcionário</li>
 		<li>Deletar funcionário</li>
 	</ul>
---
+++ src/listar.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+        <meta charset="UTF-8">
+		<title>Listar funcionários - RH</title>
+        <link type="text/css" rel="stylesheet" href="css/estilo.css";
+	</head>
+
+	<body>
+    
+	<div align="center">
+        <h1>Listar funcionários</h1>
+        <?php
+		//session_start();
+
+        $azureServer = "br-cdbr-azure-south-b.cloudapp.net";
+        $azureUser = "be848e08403198";
+        $azurePass = "59847d85";
+        
+		$conecta = mysql_connect($azureServer, $azureUser, $azurePass) or die ('Não foi possível conectar: ' . mysql_error());
+
+		if($conecta) {
+			$banco = mysql_select_db("bd_rh", $conecta) or die ('Não foi possível selecionar o banco: ' . mysql_error());;
+
+			$txt = "SELECT nm_func, email_func, tel_func, senha_func FROM tb_emp;";
+
+			$query = mysql_query ($txt, $conecta) or die ('Não foi possivel realizar query: '. mysql_error());
+
+            if(mysql_num_rows($query) <= 0){
+                echo "<h3>Funcionários não encontrados</h3><br>";
+                echo "Ao invés disso, <a href='incluir.php'>Cadastrar funcionário</a>";
+            }
+            else {
+                while()
+            }
+
+			mysql_free_result($query);
+		}
+
+		mysql_close($conecta);
+	?>
+	</div>
+
+	</body>
+</html>
