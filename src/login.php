1c72ad2153bf0ae6f8b7813b3acdf6ba8d034d66
Adilson Alves <adilson.day@hotmail.com>
2016-05-07 17:26:55 -0300

    Estilo e tela incluir
    retoques no estilo da página login e da tela de cadastro adicionada

--- src/login.php
+++ src/login.php
@@ -2,13 +2,14 @@
 <html lang="pt-br">
 	<head>
         <meta charset="UTF-8">
-		<title>Login</title>
+		<title>Login - RH</title>
         <link type="text/css" rel="stylesheet" href="css/estilo.css";
 	</head>
 
 	<body>
-
+    
 	<div align="center">
+        <h1>Entrar com um funcionário</h1>
 		<form method="POST" action="home.php">
 			<input type="text" name="txtLogin" placeholder="Nome de usuário" /> <br>
 
