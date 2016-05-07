b292183b44af81666d65bf3c5192df879d7a5b17
Adilson Alves <adilson.day@hotmail.com>
2016-05-07 18:45:10 -0300

    logica de cadastro como função

--- src/incluir.php
+++ src/incluir.php
@@ -7,10 +7,44 @@
 	</head>
 
 	<body>
+    <?php
+    
+        function cadastrar() {
+            //session_start();
+            $nome = $_POST['txtLogin'];
+            $email = $_POST['txtEmail'];
+            $tel = $_POST['txtTel'];
+            $pass = $_POST['txtSenha'];
+            
+            $azureServer = "br-cdbr-azure-south-b.cloudapp.net";
+            $azureUser = "be848e08403198";
+            $azurePass = "59847d85";
+            
+            $conecta = mysql_connect($azureServer, $azureUser, $azurePass) or die ('Não foi possível conectar: ' . mysql_error());
 
+            if($conecta) {
+                $banco = mysql_select_db("bd_rh", $conecta) or die ('Não foi possível selecionar o banco: ' . mysql_error());;
+
+                $txt = "INSERT INTO tb_emp (nm_func, email_func, tel_func, senha_func) ".
+                    "VALUES ('$nome', '$email', '$tel', '$pass');";
+
+                $query = mysql_query ($txt, $conecta) or die ('Não foi possivel realizar query: '. mysql_error());
+
+                if(mysql_affected_rows > 0){
+                    echo "<h3>Cadastro realizado</h3>";
+                    echo '$query';
+                }
+
+                mysql_free_result($query);
+            }
+
+            mysql_close($conecta);
+        }
+	?>
+    
 	<div align="center">
         <h1>Cadastrar funcionário</h1>
-		<form method="POST" action="">
+		<form method="POST" action="" onSubmit="<?php cadastrar(); ?>">
 			<input type="text" name="txtLogin" placeholder="Nome do funcionário" /> <br>
             <input type="text" name="txtEmail" placeholder="Email do funcionário" /> <br>
             <input type="text" name="txtTel" placeholder="Telefone do funcionário" /> <br>
@@ -21,36 +55,6 @@
 		</form>
 	</div>
     
-    <?php
-		//session_start();
-		$nome = $_POST['txtLogin'];
-		$email = $_POST['txtEmail'];
-        $tel = $_POST['txtTel'];
-        $pass = $_POST['txtSenha'];
-        
-        $azureServer = "br-cdbr-azure-south-b.cloudapp.net";
-        $azureUser = "be848e08403198";
-        $azurePass = "59847d85";
-        
-		$conecta = mysql_connect($azureServer, $azureUser, $azurePass) or die ('Não foi possível conectar: ' . mysql_error());
 
-		if($conecta) {
-			$banco = mysql_select_db("bd_rh", $conecta) or die ('Não foi possível selecionar o banco: ' . mysql_error());;
-
-			$txt = "INSERT INTO tb_emp (nm_func, email_func, tel_func, senha_func) ".
-                   "VALUES ('$nome', '$email', '$tel', '$pass');";
-
-			$query = mysql_query ($txt, $conecta) or die ('Não foi possivel realizar query: '. mysql_error());
-
-            if(mysql_affected_rows > 0){
-                echo "<h3>Cadastro realizado</h3>";
-                echo '$query';
-            }
-
-			mysql_free_result($query);
-		}
-
-		mysql_close($conecta);
-	?>
 	</body>
 </html>
